cc2079f6745457064a99e76e506f9f8ff5c6aceb
HengWeiBin <hengweibin1898@gmail.com>
2020-03-18 16:00:11 +0800

    added new class : GameArea, candy

---
+++ game4.10.1/Source/Candy.h
@@ -0,0 +1,20 @@
+﻿namespace game_framework
+{
+	class Candy
+	{
+	public:
+		Candy();
+		~Candy();
+		void LoadBitmap();
+		void OnMove();
+		void OnShow();
+		void SetXY(int, int);
+		bool IsAlive();
+		void SetAlive(bool);
+	private:
+		CMovingBitmap bmp;
+		bool alive;
+		int x, y;
+	};
+}
+
---
+++ game4.10.1/Source/GameArea.h
@@ -0,0 +1,21 @@
+﻿namespace game_framework
+{
+	class GameArea
+	{
+	public:
+		GameArea();
+		~GameArea();
+		void LoadBitmap();
+		void LoadStage();
+		void OnShow();
+		void OnLButtonDown(UINT nFlags, CPoint point);
+		void OnLButtonUp(UINT nFlags, CPoint point);
+		void OnMouseMove(UINT nFlags, CPoint point);
+		void InitCandy();
+		void CheckCombo();
+	private:
+		int map[13][20];
+		int x, y;	
+		Candy* candies;
+	};
+}
