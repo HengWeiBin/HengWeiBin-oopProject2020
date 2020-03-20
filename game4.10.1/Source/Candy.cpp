751dbb61df1a4de7fe578d6f767a31a97ab5bfa7
HengWeiBin <hengweibin1898@gmail.com>
2020-03-20 20:33:56 +0800

    edited candy.cpp

--- game4.10.1/Source/Candy.cpp
+++ game4.10.1/Source/Candy.cpp
@@ -30,14 +30,37 @@ namespace game_framework
 		{
 		case 1:
 			BitmapDir = ".\\Bitmaps\\RedCandy.bmp";
+			break;
 		case 2:
 			BitmapDir = ".\\Bitmaps\\OrangeCandy.bmp";
+			break;
 		case 3:
 			BitmapDir = ".\\Bitmaps\\GreenCandy.bmp";
+			break;
 		case 4:
 			BitmapDir = ".\\Bitmaps\\BlueCandy.bmp";
+			break;
 		case 5:
 			BitmapDir = ".\\Bitmaps\\PurpleCandy.bmp";
+			break;
+		/////////////////////////////////////////////////////////
+		//	Special Candy: Vertical
+		/////////////////////////////////////////////////////////
+		case 10:
+			BitmapDir = ".\\Bitmaps\\VPowRedCandy.bmp";
+			break;
+		case 20:
+			BitmapDir = ".\\Bitmaps\\VPowOrangeCandy.bmp";
+			break;
+		case 30:
+			BitmapDir = ".\\Bitmaps\\VPowGreenCandy.bmp";
+			break;
+		case 40:
+			BitmapDir = ".\\Bitmaps\\VPowBlueCandy.bmp";
+			break;
+		case 50:
+			BitmapDir = ".\\Bitmaps\\VPowPurpleCandy.bmp";
+			break;
 		}
 	}
 
@@ -48,21 +71,26 @@ namespace game_framework
 
 	void Candy::OnShow()
 	{
-
+		if (IsAlive())
+		{
+			bmp.SetTopLeft(x, y);
+			bmp.ShowBitmap();
+		}
 	}
 
-	void Candy::SetXY(int, int)
+	void Candy::SetXY(int x, int y)
 	{
-
+		this->x = x;
+		this->y = y;
 	}
 
-	bool Candy::IsAlive()
+	inline bool Candy::IsAlive()
 	{
-
+		return alive;
 	}
 
-	void Candy::SetAlive(bool)
+	void Candy::SetAlive(bool alive)
 	{
-
+		this->alive = alive;
 	}
 }
