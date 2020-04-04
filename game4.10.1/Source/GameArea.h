3efffa7cb072d0cadabb95800a219d25feb14eb0
HengWeiBin <hengweibin1898@gmail.com>
2020-04-04 19:25:38 +0800

    Revert "Update : powered candy now can be activate"
    This reverts commit 598ea4b44789b5aad257a48d3e0a90942e10af84.

--- game4.10.1/Source/GameArea.h
+++ game4.10.1/Source/GameArea.h
@@ -18,7 +18,7 @@ namespace game_framework
 		void OnLButtonDown(UINT nFlags, CPoint point);	//handle mouse click
 		void OnLButtonUp(UINT nFlags, CPoint point);	//handle mouse unclick
 		void OnMouseMove(UINT nFlags, CPoint point);	//handle mouse move
-		void InitCandy(bool drop = true);				//spawn all candies randomly
+		void InitCandy();								//spawn all candies randomly
 	private:
 		void DropCandy();
 		int ClearCombo();								//search and delete all combo
@@ -29,19 +29,11 @@ namespace game_framework
 		void GetLine(vector<Candy*>&, vector<Candy*>&, char check);						//collect candies on a same line
 		void PutCandy();								//spawn candies at spawning area
 		bool IsDropping();								//check all candies are still
-		void SwapCandy();								//Swap candies in clickedCandies
-		bool IsNeighbour(Candy&, Candy&);				//return are candies in clickedCandies is neighbour
-		void InitClickedCandy();						//unclick & clear candies in clickedCandies
+		void SwapCandy();
+		bool IsNeighbour(Candy&, Candy&);
+		void InitClickedCandy();
 		void ShowScore();
 		void ShowStarBar();
-		void Find(Candy*, unsigned&, unsigned&);		//find candy and return row and column
-		void ReleasePower(Candy*, unsigned row = 0, unsigned column = 0);				//remove candy with consider its' power
-		void ReleaseSwap();
-		void RemoveRow(unsigned);						//remove whole row of candies
-		void RemoveColumn(unsigned);					//remove whole column of candies
-		void RemoveSquare(unsigned, unsigned, int);		//remove surrounding candies according to level
-		void RemoveStyle(int style = 0);
-		void PowerAll(int, int);
 
 		int map[MaxHeight][MaxWidth];
 		int x, y;										//top left x,y of gameArea
