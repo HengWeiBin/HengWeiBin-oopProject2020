7bfabcc57ded5f2ae7185b23137870b74f5cd45f
HengWeiBin <hengweibin1898@gmail.com>
2020-04-19 21:03:22 +0800

    Update : changed jelly's color

--- game4.10.1/Source/resource.h
+++ game4.10.1/Source/resource.h
@@ -80,7 +80,6 @@
 #define IDB_YEL_VER                     217
 #define IDB_YEL_VER_C                   218
 #define IDB_YEL_PAC_C                   219
-#define IDB_BITMAP3                     220
 #define IDB_BLACK_BAR                   220
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
@@ -94,7 +93,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        221
+#define _APS_NEXT_RESOURCE_VALUE        222
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
